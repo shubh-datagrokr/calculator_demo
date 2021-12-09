eb6000e09f6431ffc9bfff069387586c4cb03c0c
shubh-datagrokr <95840565+shubh-datagrokr@users.noreply.github.com>
2021-12-09 02:20:24 -0800

    Update calc_unittest.py

--- calc_unittest.py
+++ calc_unittest.py
@@ -6,14 +6,13 @@ class TestCalculator(unittest.TestCase):
 
     def add_test(self):
         self.assertEqual(calc.add(1, 5), 6)
-    def add_test(self):
-        self.assertEqual(calc.subtract(5, 1), 4)
-    def add_test(self):
-        self.assertEqual(calc.multiply(2, 6), 12)
-    def add_test(self):
-        self.assertEqual(calc.divide(12, 4), 3)
-    def add_test(self):
-        self.assertEqual(calc.divide(12, 0), "Can not divide a number by 0.")
-    def add_test(self):
         self.assertIsNone(calc.add(12, ),)
+    def sub_test(self):
+        self.assertEqual(calc.subtract(5, 1), 4)
+    def mul_test(self):
+        self.assertEqual(calc.multiply(2, 6), 12)
+    def divide_test(self):
+        self.assertEqual(calc.divide(12, 4), 3)
+        self.assertEqual(calc.divide(12, 0), "Can not divide a number by 0.")
+        
     
