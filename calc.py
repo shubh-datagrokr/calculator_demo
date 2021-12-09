2ee5485093af9af9f3b2aac0b9194cc8c7f7df43
shubh-datagrokr <95840565+shubh-datagrokr@users.noreply.github.com>
2021-12-08 23:06:51 -0800

    create simple calc.py

---
+++ calc.py
@@ -0,0 +1,19 @@
+# calc.py
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Can not divide a number by 0.")
+    return x / y
